738aecfb096cff859176ddf887ee09a485bc2a1f
Sayed <sydr38734@gmail.com>
2024-10-13 01:06:02 +0300

    Added the status bar and dashboard

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -5,6 +5,18 @@
 ---@type ChadrcConfig
 local M = {}
 
+M.ui = {
+ statusline = {
+   theme = "minimal", -- default/vscode/vscode_colored/minimal
+   -- default/round/block/arrow separators work only for default statusline theme
+   -- round and block will work for minimal theme only
+   separator_style = "round",
+   order = nil,
+   modules = nil,
+ },
+}
+
+
 M.base46 = {
 	theme = "tokyodark",
 
@@ -14,7 +26,42 @@ M.base46 = {
 	-- },
 }
 
-M.nvdash.load_on_startup = true
+M.nvdash = {
+  load_on_startup = true,
+  header = {
+    "           ▄ ▄                   ",
+    "       ▄   ▄▄▄     ▄ ▄▄▄ ▄ ▄     ",
+    "       █ ▄ █▄█ ▄▄▄ █ █▄█ █ █     ",
+    "    ▄▄ █▄█▄▄▄█ █▄█▄█▄▄█▄▄█ █     ",
+    "  ▄ █▄▄█ ▄ ▄▄ ▄█ ▄▄▄▄▄▄▄▄▄▄▄▄▄▄  ",
+    "  █▄▄▄▄ ▄▄▄ █ ▄ ▄▄▄ ▄ ▄▄▄ ▄ ▄ █ ▄",
+    "▄ █ █▄█ █▄█ █ █ █▄█ █ █▄█ ▄▄▄ █ █",
+    "█▄█ ▄ █▄▄█▄▄█ █ ▄▄█ █ ▄ █ █▄█▄█ █",
+    "    █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█ █▄█▄▄▄█    ",
+    "                                 ",
+  },
+  -- buttons = {
+  --   { txt = "  Find File", keys = "Spc f f", cmd = "Telescope find_files" },
+  --   { txt = "  Recent Files", keys = "Spc f o", cmd = "Telescope oldfiles" },
+  --   { txt = "󰈭  Find Word", keys = "Spc f w", cmd = "Telescope live_grep" },
+  --   { txt = "󱥚  Themes", keys = "Spc t h", cmd = ":lua require('nvchad.themes').open()" },
+  --   { txt = "  Mappings", keys = "Spc c h", cmd = "NvCheatsheet" },
+  --
+  --   { txt = "─", hl = "NvDashLazy", no_gap = true, rep = true },
+  --
+  --   {
+  --     txt = function()
+  --       local stats = require("lazy").stats()
+  --       local ms = math.floor(stats.startuptime) .. " ms"
+  --       return "  Loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms
+  --     end,
+  --     hl = "NvDashLazy",
+  --     no_gap = true,
+  --   },
+  --
+  --   { txt = "─", hl = "NvDashLazy", no_gap = true, rep = true },
+  -- },
+}
 
 
 return M
