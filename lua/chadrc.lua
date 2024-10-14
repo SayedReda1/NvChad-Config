b8bfe6e46f8252d4e47efefa2e2f6c5970e9c2f9
Sayed <sydr38734@gmail.com>
2024-10-14 16:02:48 +0300

    Changed some I don't remember

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -54,34 +54,12 @@ M.nvdash = {
     "▄▄▄   ▄ ▄▄▄▄ ▄▄▄▄ ▄▄▄  ▄ ▄▄▄▄▄▄▄▄  ▄  █ █",
     "█▄█▄▄▄█▄▄▄▄█ ▄▄▄█ █ ▄  ▄ ▄▄▄▄▄▄▄█  █▄▄█ █",
     "█  ▄ ▄  ▄▄▄▄▄█  █ █ █▄▄█▄█▄█  ▄▄▄▄▄█  █ █",
-    "▄  ▄▄▄▄▄  ▄▄▄▄ ▄ ▄  ▄ ▄ ▄ ▄ ▄▄▄▄   ▄  ▄▄▄",
-    "▄  █ ▄ █  █▄▄█  ▄   █ █ █ ▄ █▄▄█▄▄▄█  █ █",
-    "█▄▄█ ▄ █▄▄▄▄█▄▄▄█▄▄▄█▄█▄█▄█ █▄▄█   ▄  █▄█",
-    "▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█",
+    -- "▄  ▄▄▄▄▄  ▄▄▄▄ ▄ ▄  ▄ ▄ ▄ ▄ ▄▄▄▄   ▄  ▄▄▄",
+    -- "▄  █ ▄ █  █▄▄█  ▄   █ █ █ ▄ █▄▄█▄▄▄█  █ █",
+    -- "█▄▄█ ▄ █▄▄▄▄█▄▄▄█▄▄▄█▄█▄█▄█ █▄▄█   ▄  █▄█",
+    -- "▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█",
     "                                         ",
   },
-  -- buttons = {
-  --   { txt = "  Find File", keys = "Spc f f", cmd = "Telescope find_files" },
-  --   { txt = "  Recent Files", keys = "Spc f o", cmd = "Telescope oldfiles" },
-  --   { txt = "󰈭  Find Word", keys = "Spc f w", cmd = "Telescope live_grep" },
-  --   { txt = "󱥚  Themes", keys = "Spc t h", cmd = ":lua require('nvchad.themes').open()" },
-  --   { txt = "  Mappings", keys = "Spc c h", cmd = "NvCheatsheet" },
-  --
-  --   { txt = "─", hl = "NvDashLazy", no_gap = true, rep = true },
-  --
-  --   {
-  --     txt = function()
-  --       local stats = require("lazy").stats()
-  --       local ms = math.floor(stats.startuptime) .. " ms"
-  --       return "  Loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms
-  --     end,
-  --     hl = "NvDashLazy",
-  --     no_gap = true,
-  --   },
-  --
-  --   { txt = "─", hl = "NvDashLazy", no_gap = true, rep = true },
-  -- },
 }
-
 
 return M
--- lua/options.lua
+++ lua/options.lua
@@ -6,4 +6,4 @@ local o = vim.o
 o.cursorlineopt ='both' -- to enable cursorline!
 o.relativenumber = true
 
-
+o.showmode = false
